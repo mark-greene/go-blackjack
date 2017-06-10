3340ebdf07eb97affe509a34f73d720902837419
Mark <mark.greene@yahoo.com>
2017-06-10 16:44:47 -0400

    Heroku

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	_ "blackjack/routers"
+	"os"
+	"strconv"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.BConfig.Listen.HTTPPort = port
+	}
 	beego.Run()
 }
